10da5b06b57e50ddd41fc8cfc19d2121f75f7acd
madmod <madmodmail@gmail.com>
2015-07-25 20:21:03 -0600

    Begin Slack integration

--- index.js
+++ index.js
@@ -2,9 +2,29 @@
 
 var config = require('./config.js');
 
+var TripCase = require('tripcase');
+var tripcase = new TripCase(config.tripcase);
+
+var slack = require('slack-notify')(config.slackWebhookUrl);
+
 
 exports.handler = function(event, context) {
   console.log('Received event:', JSON.stringify(event, null, 2));
+
+  tripcase.login(function (err, res, body) {
+    console.log('login', body);
+
+    tripcase.getTrips(function (err, res, trips) {
+      console.log('getTrips', trips);
+
+      /*
+      slack.send({
+        channel: config.channel,
+        text: ' leaving for The Pirate Bay in 3 days and will return in 1 day'
+      });
+      */
+    })
+  });
 
   context.succeed();
 };
